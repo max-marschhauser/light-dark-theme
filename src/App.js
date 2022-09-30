85498302eb34c275630ed1c19f214fa827f848bc
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-09-30 08:19:19 +0200

    light-dark button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<article>
+				<select className="theme" name="" id="">
+					<option value="dark">dark</option>
+					<option value="light">light</option>
+					<option value="rainbow">rainbow</option>
+				</select>
+
+				<p>
+					Quod fuga voluptates voluptatem inventore at, temporibus perspiciatis in cum molestiae ab nam
+					recusandae, laudantium reprehenderit, ea quis. Ea, id!
+				</p>
+				<p>
+					Velit blanditiis reprehenderit optio incidunt. Delectus, quos. Nihil quisquam repellat ex et. Dolor
+					veritatis error aspernatur esse quas! Debitis, quis!
+				</p>
+				<p>
+					Obcaecati sequi eum quasi dolorum minus consequuntur corporis excepturi perferendis officiis
+					laudantium? Dolorum ducimus reprehenderit, labore sapiente accusantium omnis veniam.
+				</p>
+				<p>
+					Tenetur quibusdam illo sunt adipisci nostrum doloribus eveniet consectetur odio labore, deleniti ad,
+					perspiciatis velit similique exercitationem, necessitatibus reprehenderit ab?
+				</p>
+				<p>
+					Quis quae beatae excepturi fuga minus reiciendis accusantium quam culpa temporibus tenetur. Itaque
+					corporis non eos fugiat, inventore maiores explicabo.
+				</p>
+				<p>
+					Magni veritatis voluptatum dolorem ea, nulla qui quasi mollitia repudiandae quod repellendus vero
+					sit blanditiis nam at iusto omnis doloribus.
+				</p>
+				<p>
+					Illo iste, harum expedita voluptas mollitia dolorem doloremque fugit aspernatur consectetur, sequi
+					reiciendis rem debitis voluptatum cumque natus repellendus eligendi?
+				</p>
+				<p>
+					Nesciunt, repellendus saepe recusandae id dolor ipsam sapiente reiciendis nemo possimus
+					exercitationem veniam culpa alias! Dolor praesentium nam quos officia!
+				</p>
+				<p>
+					Deleniti mollitia dolorum fuga. Incidunt deserunt, odit, quos laborum quaerat impedit vitae aut quod
+					eaque sequi eos. Alias, corporis rem?
+				</p>
+				<p>
+					Temporibus, consequatur laudantium. Amet iure deserunt ullam enim obcaecati quod tenetur quia magni
+					atque mollitia temporibus cupiditate, perferendis sed explicabo?
+				</p>
+				<p>
+					Maxime minima quae, quasi dolor asperiores recusandae vitae esse ducimus ipsa! Ad explicabo optio
+					sequi molestiae adipisci distinctio libero id.
+				</p>
+				<p>
+					Temporibus dolor voluptas beatae excepturi corrupti tenetur iure tempora neque illo, nisi, ea, quis
+					accusantium. Aperiam, quod quidem. Dolores, ut.
+				</p>
+				<p>
+					Hic dolorem commodi sunt at officiis consequuntur possimus molestiae pariatur veritatis. Totam,
+					tempora tenetur harum voluptates consequuntur iure quis nihil.
+				</p>
+				<p>
+					Quibusdam voluptatum optio eos a minima doloremque tempora ab necessitatibus voluptatem fuga itaque,
+					laborum nihil mollitia quas. Cumque, pariatur velit?
+				</p>
+				<p>
+					Molestias quidem id quisquam dolore qui. Corrupti, porro blanditiis eaque reiciendis, itaque nemo
+					vero, at optio saepe obcaecati eius impedit.
+				</p>
+				<p>
+					Rem quis quod nemo. Quia cumque maiores libero earum corporis labore ipsa ratione perspiciatis
+					distinctio, doloribus laudantium inventore quidem neque.
+				</p>
+				<p>
+					Tempore quisquam tenetur excepturi placeat? Nemo, consequatur? Quibusdam enim expedita repellat
+					dicta ipsam sapiente sunt, ipsa voluptatibus voluptas asperiores voluptatem!
+				</p>
+				<p>
+					Nemo quo repellendus modi deleniti totam error tenetur repudiandae debitis commodi facilis nam
+					accusamus voluptatibus veritatis, rerum corrupti voluptate eum?
+				</p>
+				<p>
+					Deleniti repellendus excepturi itaque, tempora voluptatem suscipit est quae dicta, velit commodi
+					fugit, impedit ipsa ut tempore illum deserunt corrupti!
+				</p>
+				<p>
+					Architecto quod voluptas provident impedit nisi minus voluptatum cum consequuntur maiores! Culpa
+					ipsa est adipisci distinctio, dicta iure laborum amet!
+				</p>
+				<p>
+					Ducimus, odit repudiandae quibusdam possimus voluptates natus voluptate voluptas doloremque alias
+					atque ex cupiditate adipisci suscipit nisi rem fuga facere?
+				</p>
+				<p>
+					Officia molestias, blanditiis quis harum deleniti veniam incidunt animi suscipit magni minima rem
+					aspernatur eos enim alias in obcaecati labore.
+				</p>
+				<p>
+					Perferendis aperiam quae nisi consequuntur praesentium, omnis at quod quasi reprehenderit
+					reiciendis, possimus sequi, animi est error officia iusto voluptatibus.
+				</p>
+				<p>
+					Dolore eius eligendi ullam incidunt impedit? Autem voluptatibus vero quae cum deserunt non error
+					facere eveniet, beatae expedita veniam totam?
+				</p>
+				<p>
+					Suscipit dolores non hic ducimus accusamus, aliquam totam ad quo, fugit ut quidem architecto dolorem
+					cumque fugiat commodi deleniti harum.
+				</p>
+				<p>
+					Ex error voluptatem quia. Iusto, accusantium, quas omnis consectetur quibusdam excepturi eveniet
+					assumenda corporis ab, nisi doloremque suscipit qui! Minima?
+				</p>
+				<p>
+					Odit eos maiores reiciendis, rem qui voluptatem ullam vitae dicta, quisquam repellendus dolore
+					dolorem? Eius sunt optio corrupti iste corporis?
+				</p>
+				<p>
+					Expedita eius eum, eos voluptatum explicabo laborum accusamus dolores distinctio et animi
+					necessitatibus alias dicta nisi? Autem soluta tenetur fuga?
+				</p>
+				<p>
+					Iure velit aliquid, cumque, laboriosam aspernatur commodi corrupti maxime esse dignissimos harum
+					deserunt, eligendi libero in quasi atque eos voluptatem?
+				</p>
+				<p>
+					Veniam labore ad officiis sed dignissimos totam quidem, neque quis, quisquam cumque deserunt commodi
+					modi unde. Aspernatur ab obcaecati eaque.
+				</p>
+			</article>
+		</>
+	);
 }
 
 export default App;
